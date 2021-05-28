7c466c9bf4317b9349d8fc4247cf84a0efe024fb
Alexander Bechanko <alexbechanko@gmail.com>
2021-05-28 01:07:21 -0400

    Add day01 solutions
    This adds both the first and second part solutions of day01's
    advent-of-code.

---
+++ cmd/day01.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2021 Alex Bechanko <alex.bechanko@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/alex-bechanko/advent-of-code-2020/pkg/solutions"
+	"github.com/spf13/cobra"
+)
+
+var fileInput string
+
+// day01Cmd represents the day01 command
+var day01Cmd = &cobra.Command{
+	Use:   "day01",
+	Short: "Compute the day 1 solutions for Advent of Code 2020",
+	Long:  `Computes part1 and part2 solutions for day 1 of Advent of Code 2020`,
+	Run: func(cmd *cobra.Command, args []string) {
+		solutions.Day01Solutions(fileInput)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(day01Cmd)
+	day01Cmd.Flags().StringVarP(&fileInput, "input", "i", "inputs/day01.txt", "Path to the input data")
+}
---
+++ pkg/solutions/day01.go
@@ -0,0 +1,90 @@
+/*
+Copyright © 2021 Alex Bechanko <alex.bechanko@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package solutions
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func Day01Parse(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var data []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+
+		data = append(data, num)
+	}
+
+	return data, nil
+}
+
+func Day01Solution01(data []int) (string, error) {
+	for i := 0; i+1 < len(data); i++ {
+		for j := i + 1; j < len(data); j++ {
+			if data[i]+data[j] == 2020 {
+				return strconv.Itoa(data[i] * data[j]), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("No solution found for part1")
+}
+
+func Day01Solution02(data []int) (string, error) {
+	for i := 0; i+2 < len(data); i++ {
+		for j := i + 1; j+1 < len(data); j++ {
+			for k := j + 1; k < len(data); k++ {
+				if data[i]+data[j]+data[k] == 2020 {
+					return strconv.Itoa(data[i] * data[j] * data[k]), nil
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("No solution found for part1")
+}
+
+func Day01Solutions(path string) {
+	data, err := Day01Parse(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	soln01, err := Day01Solution01(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Solution 1: %s\n", soln01)
+
+	soln02, err := Day01Solution02(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Solution 2: %s\n", soln02)
+}
