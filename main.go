90251ccf588a0e59a51b54960925778e6186d9ed
Alexander Bechanko <alexbechanko@gmail.com>
2021-05-28 15:36:56 -0400

    Remove separation of main.go and root.go
    Since all commands are defined in one place now, there isn't really a
    need for the extra structure that the `cmd` package provides. Refactored
    the code so that the contents of `cmd/root.go` are now in `main.go`.

--- main.go
+++ main.go
@@ -16,8 +16,78 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package main
 
-import "github.com/alex-bechanko/advent-of-code-2020/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/alex-bechanko/advent-of-code-2020/pkg/solutions"
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+func makeDayCommand(root *cobra.Command, day int, daySolutionFunc func(*string)) {
+	var fileInput string
+
+	cmdText := fmt.Sprintf("day%02d", day)
+	shortText := fmt.Sprintf("Compute the day %d solutions for Advent of Code 2020", day)
+	longText := fmt.Sprintf(`Computes part1 and part2 solutions for day %d of Advent of Code 2020`, day)
+
+	var dayCmd = &cobra.Command{
+		Use:   cmdText,
+		Short: shortText,
+		Long:  longText,
+		Run: func(cmd *cobra.Command, args []string) {
+			daySolutionFunc(&fileInput)
+		},
+	}
+	rootCmd.AddCommand(dayCmd)
+
+	var defaultInput = fmt.Sprintf("inputs/day%02d.txt", day)
+	dayCmd.Flags().StringVarP(&fileInput, "input", "i", defaultInput, "Path to the input data")
+}
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "advent-of-code-2020",
+	Short: "Compute solutions for Advent of Code 2020",
+	Long:  `Compute solutions for days 1-25 of Advent of Code 2020`,
+}
 
 func main() {
-	cmd.Execute()
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.advent-of-code-2020.yaml)")
+
+	makeDayCommand(rootCmd, 1, solutions.Day01Solutions)
+	makeDayCommand(rootCmd, 2, solutions.Day02Solutions)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".advent-of-code-2020" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".advent-of-code-2020")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
